bc8f5ef81625340fbee85227910a8cd6265983fd
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-09-21 00:28:44 +0530

    All working in same app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './App.css';
 
 import firebase from 'firebase/compat/app';
@@ -9,9 +9,11 @@ import 'firebase/compat/analytics';
 
 //Required Hooks
 import {useAuthState} from 'react-firebase-hooks/auth';
-import SignIn from './components/SignIn';
-import SignOut from './components/SignOut';
-import ChatRoom from './components/ChatRoom';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+// import SignIn from './components/SignIn';
+// import SignOut from './components/SignOut';
+// import ChatRoom from './components/ChatRoom';
+// import {auth} from './Auth';
 
 require('dotenv').config();
 
@@ -26,7 +28,8 @@ firebase.initializeApp({
 });
 
 const auth = firebase.auth();
-
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
 function App() {
 
@@ -45,5 +48,88 @@ function App() {
     </div>
   );
 }
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for eternity!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'}  alt=""/>
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
 
 export default App;
--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -5,19 +5,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/analytics';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
 import ChatMessage from './ChatMessage';
-
-firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
-});
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+import {auth, firestore} from '../Auth';
 
 function ChatRoom() {
     const dummy = useRef();
